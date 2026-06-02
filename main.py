2512d94e920904b2e43ca2a05ffd611d790615dd
tommy <ntommy137@gmail.com>
2026-06-02 14:56:50 -0500

    denial claims data generated

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import pandas as pd
+import glob
+
+#reading all csvs files for columns that would affect a denials claim
+for file in glob.glob("/Users/tommynguyen/codingprojects/AIhackathon/output/csv/*.csv"):
+    df = pd.read_csv(file)
+    print(f"\n--- {file} ---")
+    print(df.columns.tolist())
+
+
