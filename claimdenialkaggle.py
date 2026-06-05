2ea91bc875393a6767619d2c18dfc8bf06cdf5b2
tommy <ntommy137@gmail.com>
2026-06-05 15:14:32 -0500

    claim denial visual summary

--- claimdenialkaggle.py
+++ claimdenialkaggle.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
-from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 
 
 #TARGET COLUMN in claims.csv there is an 'outcome' column that has the column for target
@@ -36,15 +37,18 @@ stringdt_list =[
         ]
 
 
+
+#creates a binary classification for denials
 dfs["claims_main"]["binary_deny_outcome"] = np.where(dfs["claims_main"]["outcome"]=='denied', 1, 0)
 onehotencode = pd.get_dummies(dfs["claims_main"], columns = stringdt_list)
+
+
 
 #loops through feature list
 # for col in feature_list:
 #     print(f"\n=== {col} ===")
 #     print(dfs["claims_main"][col].dtype)
 
-#creates a binary classification for denials
 
 y = onehotencode["binary_deny_outcome"]
 
@@ -54,6 +58,8 @@ X = onehotencode.select_dtypes(include=['number', 'bool']).drop(["binary_deny_ou
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
+
 model = XGBClassifier()
 
 model.fit(X_train,y_train)
@@ -62,5 +68,33 @@ y_pred = model.predict(X_test)
 
 report = classification_report(y_pred, y_test)
 
+cm = confusion_matrix(y_test, y_pred)
+ConfusionMatrixDisplay(cm).plot()
+
+
+feature_importance = pd.DataFrame({
+    "feature": X.columns, 
+    "importance": model.feature_importances_
+})
+
+def get_group(feature_name):
+    for original in feature_list:
+        if feature_name.startswith(original):
+            return original
+    return feature_name
+
+feature_importance["group"]= feature_importance["feature"].apply(get_group)
+grouped = feature_importance.groupby("group")["importance"].sum()
+grouped_sorted = grouped.sort_values(ascending=False)
+
+plt.figure(figsize=(10,6)) 
+plt.barh(grouped_sorted.index, grouped_sorted.values)
+plt.tight_layout() 
+#charts chart
+plt.show()
+#shows model prediction accuracy
 print(report)
+
+
+
 
